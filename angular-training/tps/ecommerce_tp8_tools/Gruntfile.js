d120a4aaea085fba25c33079257dd15a7b26ba0a
Krazar <krazarpwnu@gmail.com>
2013-09-02 18:26:51 +0200

    minimum gruntfile & package.json for livereload and testing

---
+++ angular-training/tps/ecommerce_tp8_tools/Gruntfile.js
@@ -0,0 +1,132 @@
+// Generated on 2013-07-29 using generator-angular 0.3.1
+'use strict';
+var LIVERELOAD_PORT = 35729;
+var lrSnippet = require('connect-livereload')({ port: LIVERELOAD_PORT });
+var mountFolder = function (connect, dir) {
+    return connect.static(require('path').resolve(dir));
+};
+
+module.exports = function (grunt) {
+    // load all grunt tasks
+    require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+    // configurable paths
+    var yeomanConfig = {
+        app: 'app'
+    };
+
+    try {
+        yeomanConfig.app = require('./bower.json').appPath || yeomanConfig.app;
+    } catch (e) {
+    }
+
+    grunt.initConfig({
+        yeoman: yeomanConfig,
+        watch: {
+
+            livereload: {
+                options: {
+                    livereload: LIVERELOAD_PORT
+                },
+                files: [
+                    '<%= yeoman.app %>/{,*/}*.html',
+                    '{.tmp,<%= yeoman.app %>}/styles/{,*/}*.css',
+                    ' {.tmp,<%= yeoman.app %>}/scripts/{,*/}*.js',
+                    '<%= yeoman.app %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}'
+                ]
+            }
+        },
+        connect: {
+            options: {
+                port: 9000,
+
+                // Change this to '0.0.0.0' to access the server from outside.
+                hostname: 'localhost'
+            },
+            livereload: {
+                options: {
+                    middleware: function (connect) {
+                        return [
+                            lrSnippet,
+                            mountFolder(connect, '.tmp'),
+                            mountFolder(connect, yeomanConfig.app)
+                        ];
+                    }
+                }
+            },
+            test: {
+                options: {
+                    middleware: function (connect) {
+                        return [
+                            mountFolder(connect, '.tmp'),
+                            mountFolder(connect, 'test')
+                        ];
+                    }
+                }
+            }
+        },
+        open: {
+            server: {
+                url: 'http://localhost:<%= connect.options.port %>'
+            }
+        },
+        clean: {
+            server: '.tmp'
+        },
+        karma: {
+            unit: {
+                configFile: 'karma.conf.js',
+                singleRun: true
+            },
+            e2e: {
+                configFile: 'karma-e2e.conf.js',
+                singleRun: true
+            },
+            midway: {
+                configFile: 'karma-midway.conf.js',
+                autoWatch: false,
+                singleRun: true
+            }
+        }
+    });
+
+    grunt.registerTask('server', function (target) {
+        if (target === 'dist') {
+            return grunt.task.run(['build', 'open:server', 'connect:dist:keepalive']);
+        }
+
+        grunt.task.run([
+            'clean:server',
+            'connect:livereload',
+            'open:server',
+            'watch'
+        ]);
+    });
+
+    grunt.registerTask('test:unit', [
+        'clean:server',
+        'connect:test',
+        'karma:unit'
+    ]);
+
+    grunt.registerTask('test:e2e', [
+        'clean:server',
+        //'livereload-start',
+        'connect:livereload',
+        'karma:e2e'
+    ]);
+
+    grunt.registerTask('test:midway', [
+        'clean:server',
+        'connect:test',
+        'karma:midway'
+    ]);
+
+
+    grunt.registerTask('default', [
+
+        'test:unit',
+        //'test:e2e',
+        //'test:midway',
+    ]);
+};
