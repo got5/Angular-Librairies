a350f8e4119ee0532de31fa3d9b6bd7393485c33
krazar <pierremarot@krazar-2.local>
2014-05-23 21:07:31 +0200

    optimized site + note

--- js/services/services.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-var services = angular.module('services', []);
-
-/** Application configuration constants. */
-services.constant('configurationData', { mode: 'development', version: '1.0', angularVersion: '1.1.5' });
