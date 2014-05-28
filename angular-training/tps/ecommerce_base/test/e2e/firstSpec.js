2e5873251610b908efbe355036050e7353ab6910
ffacon <francois.facon@gmail.com>
2014-05-28 11:36:25 +0200

    Test

---
+++ angular-training/tps/ecommerce_base/test/e2e/firstSpec.js
@@ -0,0 +1,10 @@
+/**
+ * Created by pierremarot on 01/04/2014.
+ */
+
+describe("Detail Book Page", function () {
+
+    beforeEach(function () {
+        browser.get('http://localhost:3000/#/book/1');
+    });
+});
