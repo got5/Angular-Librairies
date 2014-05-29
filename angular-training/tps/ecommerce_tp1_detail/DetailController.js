52bf53424a9da1f47f53794bf3f40770a1d7e5b7
ffacon <francois.facon@gmail.com>
2014-05-29 09:54:26 +0200

    Update TP1

--- angular-training/tps/ecommerce_tp1_detail/DetailController.js
+++ angular-training/tps/ecommerce_tp1_detail/DetailController.js
@@ -1,50 +1,70 @@
-/** Product detail view controller */
-var DetailController = function($scope, $routeParams, catalogService) {
+(function () {
+    "use strict";
 
-	/** Gets product from Firebase. */
-	catalogService.getProduct($routeParams.id, $scope, "product");
-	
-	$scope.quantity = 1;
-	
-	/** Add select item to user cart. */
-	$scope.addToCart = function() {
-		// TODO
-	};
-    
-    /** Returns the CSS class for the average rating of a given product. */
-	$scope.getRatingClass = function() {
-		var productRating = 0;
-		var cssClass = "zero";
-		if ($scope.product != undefined && $scope.product.comments && $scope.product.comments.length > 0) {
-    		var sumRatings = 0;
-    		for (var index = 0; index < $scope.product.comments.length; index++) {
-    			var comment = $scope.product.comments[index];
-    			sumRatings += comment.rate;
-    		}
-    		productRating = Math.floor(sumRatings/$scope.product.comments.length);
-    		
-    		switch (productRating) {
-	    		case 5:
-	    			cssClass = "five";
-	    			break;
-	    		case 4:
-	    			cssClass = "four";
-	    			break;
-	    		case 3:
-	    			cssClass = "three";
-	    			break;
-	    		case 2:
-	    			cssClass = "two";
-	    			break;
-	    		case 1:
-	    			cssClass = "one";
-	    			break;
-	    		case 0:
-	    		default:
-	    			break;
-	    	}
-    	}
-		return "rating " + cssClass;
-	};
-};
-DetailController.$inject = ['$scope', '$routeParams', 'CatalogService'];
+    /** Product detail view controller */
+    angular.module('app')
+        .controller('DetailController', ['$scope', '$location', '$routeParams', 'catalogService',
+            function ($scope, $location, $routeParams, catalogService) {
+
+            $scope.product = {};
+
+            catalogService.getProduct($routeParams.id).success(function (result) {
+                $scope.product = result;
+            });
+
+            $scope.quantity = 1;
+
+
+
+            $scope.getImage = function (id) {
+                if (!id) {
+                    return "";
+                } else {
+                    return "/img/catalog/" + id + ".jpg";
+                }
+            };
+
+
+                /** Returns rating for a given product. */
+                var getProductRating = function(comments) {
+                    if (comments) {
+                        var sumRatings = 0;
+                        for ( var index = 0; index < comments.length; index++) {
+                            var comment = comments[index];
+                            sumRatings += comment.rate;
+                        }
+                        return Math.floor(sumRatings / comments.length);
+                    }
+                    return 0;
+                };
+
+                /** Returns the CSS class for the average rating of a given product. */
+                $scope.getCSSRating = function(pItem) {
+                    var css = ['rating'];
+
+                    if (pItem != undefined) {
+                        switch (getProductRating(pItem)) {
+                            case 1:
+                                css.push('one');
+                                break;
+                            case 2:
+                                css.push('two');
+                                break;
+                            case 3:
+                                css.push('three');
+                                break;
+                            case 4:
+                                css.push('four');
+                                break;
+                            case 5:
+                                css.push('five');
+                                break;
+                            default :
+                                css.push('zero');
+                        }
+                    }
+                    return css;
+                };
+
+        }]);
+}());
