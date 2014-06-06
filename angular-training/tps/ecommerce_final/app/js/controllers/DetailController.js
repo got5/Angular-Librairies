3a9a3fe744494fa09211dfade9d62b43de5c4466
francois.facon@gmail.com <ffacon@apache.org>
2014-06-06 23:49:00 +0200

    update detail

--- angular-training/tps/ecommerce_final/app/js/controllers/DetailController.js
+++ angular-training/tps/ecommerce_final/app/js/controllers/DetailController.js
@@ -16,5 +16,55 @@
 
 
 
+            $scope.getImage = function (id) {
+                if (!id) {
+                    return "";
+                } else {
+                    return "/img/catalog/" + id + ".jpg";
+                }
+            };
+
+
+                /** Returns rating for a given product. */
+                var getProductRating = function(comments) {
+                    if (comments) {
+                        var sumRatings = 0;
+                        for ( var index = 0; index < comments.length; index++) {
+                            var comment = comments[index];
+                            sumRatings += comment.rate;
+                        }
+                        return Math.floor(sumRatings / comments.length);
+                    }
+                    return 0;
+                };
+
+                /** Returns the CSS class for the average rating of a given product. */
+                $scope.getCSSRating = function(pItem) {
+                    var css = ['rating'];
+
+                    if (pItem != undefined) {
+                        switch (getProductRating(pItem)) {
+                            case 1:
+                                css.push('one');
+                                break;
+                            case 2:
+                                css.push('two');
+                                break;
+                            case 3:
+                                css.push('three');
+                                break;
+                            case 4:
+                                css.push('four');
+                                break;
+                            case 5:
+                                css.push('five');
+                                break;
+                            default :
+                                css.push('zero');
+                        }
+                    }
+                    return css;
+                };
+
         }]);
 }());
