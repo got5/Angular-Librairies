24fefae09832875cfb23a062a560d96684f5835f
ffacon <francois.facon@gmail.com>
2014-06-16 17:50:03 +0200

    Update Tp2

--- angular-training/tps/ecommerce_tp2_home/HomeController.js
+++ angular-training/tps/ecommerce_tp2_home/HomeController.js
@@ -1,38 +1,43 @@
-/** Home view controller */
-var HomeController = function($scope, $resource) {
-	$scope.message = "Welcome in our shop!!!";
-	
-	var News = $resource("/news/:id");
-	
-	/** Gets all the news. */
-	$scope.lstNews = News.query();
-	
-	/** Gets the news with id = 1. */
-	$scope.currentNews = News.get({ id: 1 });
-	
-	/** Creates a new news. */
-	$scope.addedNews = new News;
-	
-	/** Increments the number of likes of a news, and saves the changes. */
-	$scope.addLike = function(news) {
-		news.likes++;
-		news.$save();
-	};
-	
-	/** Deletes a news. */
-	$scope.deleteNews = function(news) {
-		news.$delete();
-		
-		$scope.lstNews = News.query();
-	};
-	
-	/** Adds a news. */
-	$scope.addNews = function() {
-		$scope.addedNews.id = -1;
-		$scope.addedNews.$save();
-		
-		// We refresh the list with the added news.
-		$scope.lstNews = News.query();
-	};
-};
-HomeController.$inject = [ '$scope', '$resource' ];
+(function () {
+    "use strict";
+
+    /** Home view controller */
+    angular.module('app')
+        .controller('HomeController', ['$scope','NewsService', function ($scope,NewsService) {
+
+            $scope.message = "Welcome in our shop!!!";
+
+
+            /** Gets all the news. */
+            $scope.lstNews = NewsService.query();
+
+            /** Gets the news with id = 1. */
+            $scope.currentNews = NewsService.get({ id: 1 });
+            $scope.newsOfTheDay = NewsService.random();
+
+            /** Creates a new news. */
+            $scope.addedNews = new NewsService();
+
+            /** Increments the number of likes of a news, and saves the changes. */
+            $scope.addLike = function(news) {
+                news.$like();
+            };
+
+            /** Deletes a news. */
+            $scope.deleteNews = function(news) {
+                news.$delete(function(){
+                    $scope.lstNews.splice($scope.lstNews.indexOf(news),1);
+                });
+
+            };
+
+            /** Adds a news. */
+            $scope.addNews = function() {
+                $scope.addedNews.$save(function(){
+                    $scope.lstNews.push($scope.addedNews);
+                    $scope.addedNews = new NewsService();
+                });
+
+            };
+        }]);
+}());
---
+++ angular-training/tps/ecommerce_tp2_home/NewsService.js
@@ -0,0 +1,10 @@
+(function(){
+    "use strict";
+    angular.module('app')
+        .factory('NewsService',['$resource', function ($resource) {
+            return $resource('/api/news/:op/:id',{id:'@id'},{
+                like: {method:'GET',params: {op:'like'}},
+                random: {method:'GET',params:{op:'random'}}
+            });
+        }]);
+}());
